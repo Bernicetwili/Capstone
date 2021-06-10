f76d473d41561fac44cf3a9c0758f4e9090783ae
Bernice Twili <bernicetwili0@gmail.com>
2021-06-10 12:02:44 +0300

    add ne comment form

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,34 @@
 from django.urls import path,include
 from rest_framework import routers
 from .import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+    FollowsListView,
+    FollowersListView,
+    postpreference,
+    post_list)
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/',views.about, name='blog-about'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/del/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>/follows', FollowsListView.as_view(), name='user-follows'),
+    path('user/<str:username>/followers', FollowersListView.as_view(), name='user-followers'),
+    path('l/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/posts', post_list),
+    path('api/users', users_list),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+import sys
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework import permissions
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from .serializers import UserSerializer, GroupSerializer
+from blog.models import Post, Comment, Preference
+from users.models import Follow, Profile
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Count
+from .forms import NewCommentForm
+from django.contrib.auth.decorators import login_required
+from rest_framework import status
 # Create your views here.
+def is_users(post_user, logged_user):
+    return post_user == logged_user
 
+
+PAGINATION_COUNT = 3
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -21,3 +35,113 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = PAGINATION_COUNT
+    
+    
+@login_required
+def postpreference(request, postid, userpreference):
+        
+        if request.method == "POST":
+                eachpost= get_object_or_404(Post, id=postid)
+
+                obj=''
+
+                valueobj=''
+
+                try:
+                        obj= Preference.objects.get(user= request.user, post= eachpost)
+
+                        valueobj= obj.value #value of userpreference
+
+
+                        valueobj= int(valueobj)
+
+                        userpreference= int(userpreference)
+                
+                        if valueobj != userpreference:
+                                obj.delete()
+
+
+                                upref= Preference()
+                                upref.user= request.user
+
+                                upref.post= eachpost
+
+                                upref.value= userpreference
+
+
+                                if userpreference == 1 and valueobj != 1:
+                                        eachpost.likes += 1
+                                        eachpost.dislikes -=1
+                                elif userpreference == 2 and valueobj != 2:
+                                        eachpost.dislikes += 1
+                                        eachpost.likes -= 1
+                                
+
+                                upref.save()
+
+                                eachpost.save()
+                        
+                        
+                                context= {'eachpost': eachpost,
+                                  'postid': postid}
+
+                                return redirect('blog-home')
+
+                        elif valueobj == userpreference:
+                                obj.delete()
+                        
+                                if userpreference == 1:
+                                        eachpost.likes -= 1
+                                elif userpreference == 2:
+                                        eachpost.dislikes -= 1
+
+                                eachpost.save()
+
+                                context= {'eachpost': eachpost,
+                                  'postid': postid}
+
+                                return redirect('blog-home')
+                                
+                        
+        
+                
+                except Preference.DoesNotExist:
+                        upref= Preference()
+
+                        upref.user= request.user
+
+                        upref.post= eachpost
+
+                        upref.value= userpreference
+
+                        userpreference= int(userpreference)
+
+                        if userpreference == 1:
+                                eachpost.likes += 1
+                        elif userpreference == 2:
+                                eachpost.dislikes +=1
+
+                        upref.save()
+
+                        eachpost.save()                            
+
+
+                        context= {'eachpost': eachpost,
+                          'postid': postid}
+
+                        return redirect('blog-home')
+
+
+        else:
+                eachpost= get_object_or_404(Post, id=postid)
+                context= {'eachpost': eachpost,
+                          'postid': postid}
+
+                return redirect('blog-home')
